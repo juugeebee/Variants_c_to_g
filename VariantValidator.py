39617604fb64930650f136cebae6798458f71f84
juugeebee <july2023@yahoo.fr>
2021-06-18 12:10:20 +0200

    batch VariantValidator

---
+++ VariantValidator.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+print('\n****************************')
+print('***** VariantValidator *****')
+print('****************************')
+
+import requests
+
+file = '/media/jbogoin/Sep2020-JGB/Donnees_brutes/hg19/Metabo/variants_introniques_17062021/15383_15029_ABCC8intronique.csv'
+output = '/media/jbogoin/Sep2020-JGB/Donnees_brutes/hg19/Metabo/variants_introniques_17062021/15383_15029_ABCC8intronique_genomique.txt'
+input_list = []
+output_list = []
+
+fichier = open(output, "a")
+
+with open(file, 'r') as filin:
+    
+    for variant_description in filin:
+    
+        link = 'https://rest.variantvalidator.org/' + '/VariantValidator/variantvalidator/' \
+            + 'hg19/' + variant_description + '/NM_001287174.1'
+
+        r = requests.get(link)
+
+        if r.status_code == 200:
+            
+            rd = r.json()
+
+            for cle, valeur in rd.items():
+                if cle.startswith('NM'):
+                    fichier.write(rd[cle]['primary_assembly_loci']['hg19']['hgvs_genomic_description'] \
+                     + '\t' + cle + '\n')
+
+        elif response.status_code == 404:
+            print('Not Found.')
+
+fichier.close()
+
+print('****************************')
+print('******* JOB DONE ! *********')
+print('****************************\n')
+     
+
